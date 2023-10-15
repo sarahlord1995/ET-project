dfe25cf15d28a7187ef41198a632d0625ae914e9
mahmudfarsi <mhfarsi002022@gmail.com>
2023-10-15 15:07:15 +0330

    creat function for like

--- js/main.js
+++ js/main.js
@@ -1,15 +1,26 @@
 
-
-
-
 const $ = document;
 const header = $.querySelector('header');
-
-
 window.addEventListener('scroll',()=>{
   if(document.documentElement.scrollTop > 20 || document.scrollTop > 20){
     header.classList.add('show');
   }else{
     header.classList.remove('show');
   }
+})
+
+
+const heart =  $.querySelectorAll('.heart-icon');
+let flage = true;
+
+heart.forEach(item=>{
+  item.addEventListener('click',()=>{
+    if(flage){
+      item.src = './assets/icons/Vector (3).svg';
+      flage = false;
+    }else{
+      item.src = 'assets/icons/fi-rr-heart.svg';
+      flage = true;
+    }
+  })
 })
